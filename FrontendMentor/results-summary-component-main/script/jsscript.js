f11d783515eb56c04f02bb4c1dc3d8587676667d
Ryddel D Guervens Nacius <gf291989@ens.uqam.ca>
2024-01-15 01:36:56 -0500

    (Responsive complete) Add CSS code for responsive

--- FrontendMentor/results-summary-component-main/script/jsscript.js
+++ FrontendMentor/results-summary-component-main/script/jsscript.js
@@ -1,12 +1,10 @@
 const tableau = new Array("reaction", "memory", "verbal", "visual");
-
 async function jsonData() {
   try {
     const response = await fetch("./data.json");
     const eltJson = await response.json();
 
     let variable = eltJson;
-    // console.log("dans le fetch :", variable);
     return variable;
   } catch (error) {
     console.error("Erreur de lecture JSON:", error);
@@ -20,6 +18,7 @@ jsonData().then((elt) => {
 async function select_item() {
   const elmt = await jsonData();
 
+  let moyTotal = 0;
   const tableau = new Array("reaction", "memory", "verbal", "visual");
   for (var i = 0; i < tableau.length; i++) {
     let img = document.createElement("img");
@@ -28,6 +27,7 @@ async function select_item() {
     let p_score = document.createElement("p");
     let texte_p_category = document.createTextNode(elmt[i].category);
     let texte_p_score = document.createTextNode(elmt[i].score + " / 100");
+    moyTotal += elmt[i].score;
     img.setAttribute("src", elmt[i].icon);
     item.appendChild(img);
     p_category.appendChild(texte_p_category);
@@ -35,6 +35,10 @@ async function select_item() {
     p_score.appendChild(texte_p_score);
     item.appendChild(p_score);
   }
+  moyTotal = Math.round(moyTotal / tableau.length);
+  console.log(moyTotal);
+  let result = document.getElementsByClassName("summary")[0];
+  result.innerHTML = moyTotal;
 }
 
-select_item();
+window.addEventListener("load", select_item);
